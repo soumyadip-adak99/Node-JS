8e8b86dde72f0dcc4166da034dd1e5f1fc6c45bc
Soumya ! <work.soumyadipadak@gmail.com>
2025-09-29 13:33:06 +0530

    utils code

---
+++ Express js/Backend-project-01/src/utils/asyncHandler.js
@@ -0,0 +1,24 @@
+// ================= promises method ====================
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
+    }
+}
+
+
+// =============== async  method ===============
+/* 
+const asyncHandler = (func) => async (req, res, next) => {
+    try {
+        await func(req, res, next)
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+    }
+} 
+*/
+
+export { asyncHandler }
