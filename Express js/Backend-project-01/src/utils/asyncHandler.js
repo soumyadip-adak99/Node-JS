d181f4ded82b4cd055f4b5b2dd90dab71939a4e9
Soumya ! <work.soumyadipadak@gmail.com>
2025-09-30 09:51:23 +0530

    all setup configure

--- Express js/Backend-project-01/src/utils/asyncHandler.js
+++ Express js/Backend-project-01/src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 // ================= promises method ====================
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
     }
 }
